a76dcab06346966e53a35223f10ee3333c5d770c
Palc3301 <pedroarruda8@live.com>
2024-07-17 13:09:32 -0300

    Feat-PackageJS

---
+++ vs14-back/03-Front/task04/js/exer01.js
@@ -0,0 +1,5 @@
+/*Você foi contratado para criar um programa em JavaScript que converta uma temperatura fornecida em graus Celsius para graus Fahrenheit. Utilize as fórmulas de conversão apropriadas para realizar essa tarefa. O programa deve declarar uma constante (celsiusTemperature) com o valor da temperatura em graus Celsius e, em seguida, calcular e exibir a correspondente temperatura em graus Fahrenheit.
+*/
+const celsiusTemperature = 25;
+const fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+console.log(`A temperatura de ${celsiusTemperature}°C corresponde a ${fahrenheitTemperature}°F.`);
