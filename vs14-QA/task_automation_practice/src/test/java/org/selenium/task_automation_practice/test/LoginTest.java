08358965c385ca8a09f4e8d2007f51bdb5d16b8c
Tiago Souto <tiagosoutoma@gmail.com>
2024-08-22 10:59:34 -0300

    feat: primeiro teste com sucesso

--- vs14-QA/task_automation_practice/src/test/java/org/selenium/task_automation_practice/test/LoginTest.java
+++ vs14-QA/task_automation_practice/src/test/java/org/selenium/task_automation_practice/test/LoginTest.java
@@ -1,7 +1,13 @@
 package org.selenium.task_automation_practice.test;
 
+import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
+import org.junit.jupiter.api.Test;
+import org.selenium.task_automation_practice.data.LoginData;
+import org.selenium.task_automation_practice.dto.LoginDto;
+import org.selenium.task_automation_practice.page.LoginPage;
+import org.selenium.task_automation_practice.selenium.Validation;
 
 import static storys.LoginStory.*;
 
@@ -9,4 +15,14 @@ import static storys.LoginStory.*;
 @Story(USER_STORY_LOGIN)
 public class LoginTest extends BaseTest {
 
+    LoginPage loginPage = new LoginPage();
+    LoginData loginData = new LoginData();
+    Validation validation = new Validation();
+
+    @Test
+    public void test1validarLoginDadosValidos(){
+        LoginDto usuario =  loginData.loginDadosValidos();
+        String mensagem = loginPage.fazerLogin(usuario.getEmail(), usuario.getSenha());
+        validation.validateText("MY ACCOUNT", mensagem);
+    }
 }
